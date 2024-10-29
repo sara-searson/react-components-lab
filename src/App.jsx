1718de4df6695400c831a495afb414bbb2cedd47
sara-searson <sara.r.searson@gmail.com>
2024-10-29 15:30:46 -0400

    finished lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,61 @@
+import './App.css'
+import WeatherForecast from "./WeatherForecast";
+
+const weatherForecasts = [
+  {
+    key: 1,
+    day: 'Mon',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg',
+    imgAlt: 'sun icon',
+    conditions: 'sunny',
+    time: 'Morning',
+  },
+  {
+    key: 2,
+    day: 'Tue',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg',
+    imgAlt: 'moon icon',
+    conditions: 'clear',
+    time: 'Night',
+  },
+  {
+    key: 3,
+    day: 'Wed',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg',
+    imgAlt: 'clouds with lightning icon',
+    conditions: 'stormy',
+    time: 'All Day',
+  },
+  {
+    key: 4,
+    day: 'Thu',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg',
+    imgAlt: 'sun overcast by clouds icon',
+    conditions: 'overcast',
+    time: 'Evening',
+  },
+  {
+    key: 5,
+    day: 'Fri',
+    img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg',
+    imgAlt: 'moon overcast by clouds icon',
+    conditions: 'cloudy',
+    time: 'Night',
+  },
+];
+
+
 const App = () => {
 
   return (
-    <h1>Hello world!</h1>
+    <div className="body">
+      <h1>Local Weather</h1>
+      <section>
+      {weatherForecasts.map((forecast) => (
+        <WeatherForecast key={forecast.id} forecast={forecast} />
+      ))}
+      </section>
+    </div>
   );
 }
 
